b87dccdb97ac016007096b51cc1aa08a396c7404
mqbssppe <panagiotis.papastamoulis@manchester.ac.uk>
2017-09-22 09:49:34 +0100

    edit

--- toy_example.R
+++ toy_example.R
@@ -5,28 +5,28 @@ source('BayesDRIMSeq.R')
 nGenes <- 100
 genes <- c()
 for(i in 1:nGenes){
-	nIso <- 1 + rpois(n=1,  lambda = 2)	# number of transcripts per gene
+	nIso <- 1 + rpois(n=1,  lambda = 2)		# number of transcripts per gene
 	genes <- c(genes, rep(paste0("GENE_",i), nIso))
 }
 genes <- as.factor(genes)
 
-nTranscripts <- length(genes)		# total number of transcripts
-nSamplesA <- 3				# number of replicates for 1st condition 
-nSamplesB <- 3				# number of replicates for 2nd condition
-nSamples <- nSamplesA + nSamplesB	# total number of replicates
+nTranscripts <- length(genes)				# total number of transcripts
+nSamplesA <- 3						# number of replicates for 1st condition 
+nSamplesB <- 3						# number of replicates for 2nd condition
+nSamples <- nSamplesA + nSamplesB			# total number of replicates
 # 	Simulate data.frame with counts (no DTU evidence at all)
 countDataFrame <- data.frame( matrix(rpois(n = nTranscripts*nSamples, lambda = 30), nrow = nTranscripts, ncol = nSamples) )
 
 # 	Call the BayesDRIMSEQ function as:
 myRes <- laplaceDM(
-	count_data = countDataFrame, 			# data.frame of counts with dimension: nTranscripts x nSamples
-	gene_data = genes, 				# factor with `nGenes` levels with length: nTranscripts
-	grouping = as.factor(
-		c(rep('A',nSamplesA),
-			rep('B',nSamplesB))), 		# factor with 2 levels and length nSamples
-	min_reads_filter = 20, 				# positive integer used to filter our low expressed transcripts
-	nCores = 8, 					# number of paraller workers
-	lambdaRate = 0.5				# positive prior parameter \lambda
+		count_data = countDataFrame, 		# data.frame of counts with dimension: nTranscripts x nSamples
+		gene_data = genes, 			# factor with `nGenes` levels with length: nTranscripts
+		grouping = as.factor(
+			c(rep('A',nSamplesA),
+				rep('B',nSamplesB))), 	# factor with 2 levels and length nSamples
+		min_reads_filter = 20, 			# positive integer used to filter our low expressed transcripts
+		nCores = 8, 				# number of paraller workers
+		lambdaRate = 0.5			# positive prior parameter \lambda
 	)
 
 
